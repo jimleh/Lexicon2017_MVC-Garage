5e31748ac1a4e98b45dc19a0d809f6ea2aaee7ae
jimleh <jimmy.lehtola@gmail.com>
2017-10-30 09:44:05 +0100

    Added the Vehicle models, currently using strings instead of enums.

---
+++ MVCGarage/Models/VehicleModels.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCGarage.Models
+{
+    public class Vehicle
+    {
+        public string Type { get; set; }
+        public string RegistrationNumber { get; set; }
+        public string Date { get; set; }
+        public string Spot { get; set; }
+        public int Size { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+
+
+    public class MC : Vehicle
+    {
+        public MC()
+        {
+            Type = "MC";
+            Size = 1;
+        }
+    }
+    public class Car : Vehicle
+    {
+        public Car()
+        {
+            Type = "Car";
+            Size = 1;
+        }
+    }
+    public class BUS : Vehicle
+    {
+        public BUS()
+        {
+            Type = "BUS";
+            Size = 3;
+        }
+    }
+    public class Truck : Vehicle
+    {
+        public Truck()
+        {
+            Type = "Truck";
+            Size = 3;
+        }
+    }
+}

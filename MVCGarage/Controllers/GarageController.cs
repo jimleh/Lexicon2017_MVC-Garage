012b32c6a8f95bd7700f146cc2a23e7d9ad166ab
capsizedmoose <capsizedmoose@gmail.com>
2017-10-30 10:12:58 +0100

    Skapade garagecontroller med lite views som ska finnas

---
+++ MVCGarage/Controllers/GarageController.cs
@@ -0,0 +1,44 @@
+﻿using MVCGarage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCGarage.Controllers
+{
+    public class GarageController : Controller
+    {
+        // GET: Garage
+        // Här Ska Listan av alla parkerade bilar + nr av öppna platser.
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: add
+        public ActionResult Add()
+        {
+            return View();
+        }
+        // POST: add
+        // Skapa en Form som sparar en parkering.
+        public ActionResult Add(Vehicle vehicle)
+        {
+            return View(vehicle);
+        }
+
+        // GET: remove
+        public ActionResult Remove()
+        {
+            return View();
+        }
+        // POST: remove
+        // Skapa en Form som sparar en parkering.
+        public ActionResult Remove(Vehicle vehicle)
+        {
+            return View(vehicle);
+        }
+
+    }
+}

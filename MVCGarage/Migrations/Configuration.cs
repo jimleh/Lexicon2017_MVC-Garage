732d38cd62cd92c00db4fd52a5a7e9d7e2670485
jimleh <jimmy.lehtola@gmail.com>
2017-11-02 10:30:51 +0100

    Added new seeds to add to the database.

--- MVCGarage/Migrations/Configuration.cs
+++ MVCGarage/Migrations/Configuration.cs
@@ -2,7 +2,6 @@ namespace MVCGarage.Migrations
 {
     using MVCGarage.Models;
     using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
@@ -15,21 +14,44 @@ namespace MVCGarage.Migrations
 
         protected override void Seed(MVCGarage.GarageContext context)
         {
-            context.Vehicles.AddOrUpdate(
-                v=>v.ParkingID,
-                new Vehicle
-                {
-                    ParkingID = 1,
-                    Type = VehicleType.Car,
-                    RegistrationNumber = "111-111",
-                    DateParked = DateTime.Now.ToString(),
-                    ParkingSpot = 1,
-                    Size=3,
-                    Owner="Christine"
+            for (int i = 1; i <= 10; i++)
+            {
+                context.Vehicles.AddOrUpdate(
+                    v => v.ParkingID,
+                    new Vehicle
+                    {
+                        ParkingID = i,
+                        Type = VehicleType.Car,
+                        RegistrationNumber = "111-" + i.ToString() + i.ToString() + i.ToString(),
+                        DateParked = DateTime.Now.ToString(),
+                        ParkingSpot = i,
+                        Size = 3,
+                        Owner = "Christine" + i.ToString()
 
-                }
-                
+                    }
                 );
+            }
+
+            int index = 1;
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 25; j++)
+                {
+                    bool occupied;
+                    if (context.Vehicles.FirstOrDefault(v => v.ParkingSpot == index) != null)
+                    {
+                        occupied = true;
+                    }
+                    else
+                    {
+                        occupied = false;
+                    }
+                    context.ParkingSpots.AddOrUpdate(
+                        s => s.ID,
+                        new ParkingSpot { ID = index++, X = i, Y = j, Occupied = occupied }
+                        );
+                }
+            }
         }
     }
 }

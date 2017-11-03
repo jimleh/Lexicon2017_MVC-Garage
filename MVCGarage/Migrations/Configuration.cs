ce6827381c08ea856816a70ca6e86630a2523a5d
jimleh <jimmy.lehtola@gmail.com>
2017-11-03 13:16:14 +0100

    Fixed ParkingSpotCheckFree (threw an IndexOutOfRangeException)

--- MVCGarage/Migrations/Configuration.cs
+++ MVCGarage/Migrations/Configuration.cs
@@ -4,6 +4,7 @@ namespace MVCGarage.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using MVCGarage.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<MVCGarage.GarageContext>
     {
